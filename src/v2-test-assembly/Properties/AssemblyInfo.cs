5e122d9267a6de1b3c4d0370f0684efc9a1ee11b
Charlie Poole <charliepoole@gmail.com>
2016-08-31 13:15:29 -0700

    Rename V2 test assembly

--- src/v2-test-assembly/Properties/AssemblyInfo.cs
+++ src/v2-test-assembly/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mock-v2-assembly")]
+[assembly: AssemblyTitle("v2-test-assembly")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mock-v2-assembly")]
+[assembly: AssemblyProduct("v2-test-assembly")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
